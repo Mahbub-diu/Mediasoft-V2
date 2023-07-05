55ce5ec412f57a4d7237348b2fcaf0564b5ed032
Mahbub-diu <mahbub.mediasoft@gmail.com>
2023-07-05 18:57:03 +0600

    up

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,5 @@
 // this theme is designed & developed for Mediasoft Data Systems Ltd. By Mahbubur Rahman on July 2023. Do not copy any Part or any content from this site. All rights reserved to Mediasoft Data Systems Ltd.
+
+$("[data-background").each(function () {
+  $(this).css("background-image", "url( " + $(this).attr("data-background") + "  )");
+});
